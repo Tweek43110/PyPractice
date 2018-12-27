121599efca60717288596b1d307290943eb7ef56
Alex <alex.r.coe@gmail.com>
2018-12-27 15:25:05 -0600

    Alittle bit of python practice today, more focus on JS

---
+++ Module Practice/myLibrary.py
@@ -0,0 +1,8 @@
+def sayHello(name):
+    print("Hello", name)
+    return
+
+def sayGoodbye(name):
+    print("Goodbye", name)
+    return
+
---
+++ More fun formatting with strings.py
@@ -0,0 +1,11 @@
+print('Hello there (single quote)')
+print("Hello there (Double quote")
+print("""This is a multiple line
+string using triple double quotes.
+You can also use triple single quotes.""")
+
+print("Part of this code \r\nis on the next line.")
+print("This is an A with a grave accent: \xC0")
+print("This is a drawing character: \u2562")
+print("This is a pilcrow: \266.")
+print("This is a division sign: \xF7")
